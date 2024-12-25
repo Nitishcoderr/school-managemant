e42aa03f1c2edbf28ebdd717d47730ff134e1e57
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2024-12-25 18:13:17 +0530

    admin-role card created

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import UserCard from '@/components/UserCard';
+import React from 'react';
 
 const AdminPage = () => {
   return (
-    <div>
-      Admin
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* UserCard */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* right */}
+      <div className="flex gap-4 justify-between flex-wrap"></div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
